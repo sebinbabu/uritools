d3b1b5e31ef759898139241d47c2edbaa073dc5c
Sebin Babu <sebin1998@gmail.com>
2018-08-24 17:30:42 +0530

    added uri check and reconstruction

--- example.c
+++ example.c
@@ -9,15 +9,20 @@ int main(int argc, char **argv)
 	char buf[20], *path;
 	char *uri_string = argv[1];
 
-	if (uri_parse(&u, uri_string) == -1) {
+	if (uri_check(uri_string) == 1) {
 		fprintf(stderr, "Could not parse uri!\n");
 		return 1;
+	} else {
+		if(uri_parse(&u, uri_string) == -1)
+			return 1;
 	}
 
 	fprintf(stdout, "uri Values:\n");
 	fprintf(stdout, "\tprotocol:\t%s\n", u.scheme);
 	fprintf(stdout, "\thost:\t\t%s\n", u.host);
-	fprintf(stdout, "\tport:\t\t%d\n", u.port);
+	fprintf(stdout, "\tpassword:\t%s\n", u.username);
+	fprintf(stdout, "\tusername:\t%s\n", u.password);
+	fprintf(stdout, "\tport:\t\t%s\n", u.port);
 	fprintf(stdout, "\tpath:\t\t%s\n", u.path);
 	fprintf(stdout, "\tquery:\t\t%s\n", u.query);
 	fprintf(stdout, "\tfragment:\t%s\n", u.fragment);
@@ -26,6 +31,8 @@ int main(int argc, char **argv)
 
 	fprintf(stdout, "Split paths:\n");
 	while(uri_path_split(buf, &path) > 0)
-		printf("\t%s\n", buf);
+		fprintf(stdout, "\t%s\n", buf);
+	fprintf(stdout, "Actual URI:\n\t%s\n", uri_reconstruct(&u));
+	
 	return 0;
 }
